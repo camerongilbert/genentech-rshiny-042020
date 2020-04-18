78f1e163c53a32e2453efd577b3e64d70a5ec99d
Cameron Gilbert <cam.d.gilbert@gmail.com>
2020-04-18 15:55:27 -0700

    Initial app structure and first UI element

--- app.r
+++ app.r
@@ -28,3 +28,78 @@ colnames(labVals) <- c("studyId",
                        "value",
                        "unit",
                        "visit")
+
+demoPlots <- c("age", "race", "sex", "sexAge", "sexRace", "ageRace")
+names(demoPlots) <- c("Age", "Race", "Sex", "Age by Sex", "Race by Sex", "Age by Race")
+
+ui <- fluidPage(
+  titlePanel('R Shiny Exercise: Cameron Gilbert'),
+  hr(),
+  
+  #The demographic summary area allows the user to see high-level information
+  #about the patients that participated in the study. 
+  
+  #Plots are set to display at the click of a button, and the user
+  #can download the plot with a second button. 
+  
+  h2('Demographic summary'),
+  
+  sidebarLayout(
+    sidebarPanel(radioButtons("demArm", "Which arm to examine?", 
+                              selected = "All",
+                              inline = FALSE, width = NULL, 
+                              choiceNames = c("All", "Drug X", "Placebo", "Combination"),
+                              choiceValues = c("all", "ARM A", "ARM B", "ARM C")),
+                 
+                 uiOutput("demArmOrOverall"),
+                 
+                 selectInput("demType",
+                             label = "Choose a plot to examine",
+                             choices = demoPlots, width = "50%",
+                             selected = "age",
+                             multiple = F
+                 ),
+                 
+                 actionButton("goDemo", label = "Update Demographics"),
+                 hr(),
+                 downloadButton("downPlot1", label = "Download Plot")
+    ),
+    
+    mainPanel(plotOutput('demSum'))
+  )
+    
+)
+
+
+
+
+server <- function(input, output) {
+  
+  rv <- reactiveValues(armToPlot = "all",
+                       armOrOverall = "Overall")
+  
+  ## Demographic summary
+  
+  
+  observeEvent(input$demArm, { rv$armToPlot <- input$demArm })
+  
+  output$demArmOrOverall <- renderUI({
+    if(rv$armToPlot == "All"){
+      radioButtons("armOrOverall", "Plot overall demographics or break down by study arm?",
+                   choices = c("Overall", "By Arm"), selected = "Overall")
+    }
+  })
+  observeEvent(input$armOrOverall, {rv$armOrOverall <- input$armOrOverall})
+  
+  
+  demPlotInput <- eventReactive(
+    input$goDemo, {
+      
+    }
+  )
+  
+  
+}
+
+
+shinyApp(ui = ui, server = server)
